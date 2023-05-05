26f439d3aed4b83e7e292f547923fdd8e66b1739
Per Arnborg <kontakt@perarnborg.se>
2023-05-05 16:35:19 +0200

    Fix constant name

--- akka-headless-wp.php
+++ akka-headless-wp.php
@@ -30,10 +30,10 @@ define('AKKA_CMS_MEDIA_BUCKET_BASE', getenv('AKKA_CMS_MEDIA_BUCKET_HOSTNAME') ? 
 define('AKKA_FRONTEND_FLUSH_CAHCE_ENDPOINT', '/api/cache');
 define('AKKA_FRONTEND_FLUSH_CACHE_KEY', getenv('AKKA_FRONTEND_FLUSH_CACHE_KEY') ? getenv('AKKA_FRONTEND_FLUSH_CACHE_KEY') : "");
 
-require_once(AKKA_DIR . 'includes/ahw-utils.php');
-require_once(AKKA_DIR . 'includes/ahw-blocks.php');
-require_once(AKKA_DIR . 'includes/ahw-content.php');
-require_once(AKKA_DIR . 'includes/ahw-acf-fields.php');
-require_once(AKKA_DIR . 'public/ahw-hooks.php');
-require_once(AKKA_DIR . 'public/ahw-api-endpoints.php');
-require_once(AKKA_DIR . 'public/ahw-healthz.php');
+require_once(AKKA_HEADLESS_WP_DIR . 'includes/ahw-utils.php');
+require_once(AKKA_HEADLESS_WP_DIR . 'includes/ahw-blocks.php');
+require_once(AKKA_HEADLESS_WP_DIR . 'includes/ahw-content.php');
+require_once(AKKA_HEADLESS_WP_DIR . 'includes/ahw-acf-fields.php');
+require_once(AKKA_HEADLESS_WP_DIR . 'public/ahw-hooks.php');
+require_once(AKKA_HEADLESS_WP_DIR . 'public/ahw-api-endpoints.php');
+require_once(AKKA_HEADLESS_WP_DIR . 'public/ahw-healthz.php');
