272fa5822565509a94c30e74e7493ee34dbb399f
Per Arnborg <kontakt@perarnborg.se>
2023-05-31 15:47:28 +0200

    v0.2.9

--- akka-headless-wp.php
+++ akka-headless-wp.php
@@ -5,7 +5,7 @@ Plugin URI: https://github.com/aventyret/akka-wp/blob/main/plugins/akka-headless
 Description: Use Wordpress as a headless CMS, with Gutenberg as the content provider
 Author: Mediakooperativet, Äventyret
 Author URI: https://aventyret.com
-Version: 0.2.2
+Version: 0.2.9
 */
 
 if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)){
@@ -19,7 +19,7 @@ if (defined('AKKA_HEADLESS_WP'))
 
 define('AKKA_HEADLESS_WP',  __FILE__ );
 define('AKKA_HEADLESS_WP_DIR', plugin_dir_path( __FILE__ ));
-define('AKKA_HEADLESS_WP_VER', "0.2.2");
+define('AKKA_HEADLESS_WP_VER', "0.2.9");
 define('AKKA_API_BASE', "headless/v1");
 define('AKKA_CMS_COOKIE_PATH', getenv('AKKA_CMS_COOKIE_PATH') ? getenv('AKKA_CMS_COOKIE_PATH') : NULL);
 define('AKKA_CMS_COOKIE_NAME', getenv('AKKA_CMS_COOKIE_NAME') ? getenv('AKKA_CMS_COOKIE_NAME') : "cms_signed_in");
