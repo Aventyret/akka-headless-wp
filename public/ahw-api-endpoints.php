c5ca9853796ebf3ed0d7f528916ad19f68a94ed1
ivarbergman <ivar.bergman@aventyret.com>
2023-12-04 14:39:38 +0100

    Add _ characer to slug regex.

--- public/ahw-api-endpoints.php
+++ public/ahw-api-endpoints.php
@@ -5,7 +5,7 @@ add_action( 'rest_api_init', function () {
     'callback' => 'Akka_headless_wp_content::get_site_meta',
     'permission_callback' => 'Akka_headless_wp_content::can_get_content',
   ) );
-  register_rest_route( AKKA_API_BASE, '/post/(?P<permalink>[a-zA-Z0-9-%+]+)', array(
+  register_rest_route( AKKA_API_BASE, '/post/(?P<permalink>[a-zA-Z0-9-%+_]+)', array(
     'methods' => 'GET',
     'callback' => 'Akka_headless_wp_content::get_post',
     'permission_callback' => 'Akka_headless_wp_content::can_get_content',
@@ -25,17 +25,17 @@ add_action( 'rest_api_init', function () {
     'callback' => 'Akka_headless_wp_content::get_attachment_by_id',
     'permission_callback' => 'Akka_headless_wp_content::can_get_content',
   ) );
-  register_rest_route( AKKA_API_BASE, '/term/(?P<taxonomy_slug>[a-zA-Z0-9-%+]+)/(?P<term_slug>[a-zA-Z0-9-%+]+)', array(
+  register_rest_route( AKKA_API_BASE, '/term/(?P<taxonomy_slug>[a-zA-Z0-9-%+_]+)/(?P<term_slug>[a-zA-Z0-9-%+_]+)', array(
     'methods' => 'GET',
     'callback' => 'Akka_headless_wp_content::get_term',
     'permission_callback' => 'Akka_headless_wp_content::can_get_content',
   ) );
-  register_rest_route( AKKA_API_BASE, '/author/(?P<author_slug>[a-zA-Z0-9-%+]+)', array(
+  register_rest_route( AKKA_API_BASE, '/author/(?P<author_slug>[a-zA-Z0-9-%+_]+)', array(
     'methods' => 'GET',
     'callback' => 'Akka_headless_wp_content::get_author',
     'permission_callback' => 'Akka_headless_wp_content::can_get_content',
   ) );
-  register_rest_route( AKKA_API_BASE, '/search/(?P<query>[a-zA-Z0-9-%+]+)', array(
+  register_rest_route( AKKA_API_BASE, '/search/(?P<query>[a-zA-Z0-9-%+_]+)', array(
     'methods' => 'GET',
     'callback' => 'Akka_headless_wp_content::search',
     'permission_callback' => 'Akka_headless_wp_content::can_get_content',
