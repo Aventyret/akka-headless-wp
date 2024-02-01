20ae280c61e7b11642d1d01acb4d8a3c71cdb1a3
Per Arnborg <kontakt@perarnborg.se>
2024-02-02 00:29:21 +0100

    Allow search with no query

--- public/ahw-api-endpoints.php
+++ public/ahw-api-endpoints.php
@@ -40,4 +40,9 @@ add_action( 'rest_api_init', function () {
     'callback' => 'Akka_headless_wp_content::search',
     'permission_callback' => 'Akka_headless_wp_content::can_get_content',
   ) );
+  register_rest_route( AKKA_API_BASE, '/search', array(
+    'methods' => 'GET',
+    'callback' => 'Akka_headless_wp_content::search',
+    'permission_callback' => 'Akka_headless_wp_content::can_get_content',
+  ) );
 } );
