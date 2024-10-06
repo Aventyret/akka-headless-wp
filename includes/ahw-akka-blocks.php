c7dd455a34fe61bfbccf8d65df5cde0b1f6a8bd1
Per Arnborg <kontakt@perarnborg.se>
2024-10-06 21:07:57 +0200

    SPLX akka blocks (#14)
    * Try splx akka blocks
    
    * Fis akka splx blocks
    
    * Fix splx akka blocks

--- includes/ahw-akka-blocks.php
+++ includes/ahw-akka-blocks.php
@@ -37,6 +37,42 @@ class Akka_headless_wp_akka_blocks {
     });
   }
 
+  public static function register_splx_block_type($block_type, $args = []) {
+    if (!Resolvers::resolve_field($args, "akka_component_name")) {
+        throw new Exception("Missing akka component name for Solarplexus block " . $block_type);
+    }
+
+    if (!Resolvers::resolve_field($args, "block_props_callback")) {
+        // If no props callback is provided, props are the same as block attributes
+        throw new Exception("Missing block props callback for Solarplexus block " . $block_type);
+        return;
+    }
+
+    Solarplexus_Helpers::use_custom_editor_ssr_component();
+
+    if (is_admin()) {
+        return;
+    }
+
+    self::$akka_blocks["splx/" . $block_type] = $args;
+
+    add_filter("splx_block_args", function ($splx_args, $splx_block_type) use($block_type, $args) {
+        if ($splx_block_type != $block_type) {
+            return $splx_args;
+        }
+        return array_merge($splx_args, ["props" => $args["block_props_callback"]($splx_args)]);
+    }, 10, 2);
+
+    add_filter("splx_block_render_callback", function ($template, $splx_args, $splx_block_type) use($block_type, $args) {
+        if ($splx_block_type != $block_type) {
+            return $template;
+        }
+        return '<div data-akka-component="' . $args["akka_component_name"] . '" data-akka-props="' .
+            rawurlencode(json_encode(Resolvers::resolve_array_field($splx_args, "props"), JSON_FORCE_OBJECT)) .
+            '"></div>';
+    }, 10, 3);
+  }
+
   private static function get_block_props($block_type, $block_attributes, $block_content = NULL) {
     if (!isset(self::$akka_blocks[$block_type])) {
         throw new Exception("Missing registration for Akka block " . $block_type);
@@ -49,6 +85,20 @@ class Akka_headless_wp_akka_blocks {
     // Add block content as children prop for the frontent
     if ($block_content) {
         $props = array_merge($props, ["children" => $block_content]);
+    }
+    return $props;
+  }
+
+  private static function get_splx_block_props($block_type, $block_attributes) {
+    if (!isset(self::$akka_blocks[$block_type])) {
+        throw new Exception("Missing registration for Akka solarplexus block " . $block_type);
+    }
+    $props = $block_attributes;
+    // Get props from callback, if one is registered with the block
+    if (isset(self::$akka_blocks[$block_type]["block_props_callback"])) {
+        $block_config = Solarplexus_Helpers::retrieve_block_config(str_replace("splx/", "", $block_type));
+        $splx_args = Solarplexus_Helpers::block_args($block_config, $block_attributes);
+        $props = self::$akka_blocks[$block_type]["block_props_callback"]($splx_args);
     }
     return $props;
   }
@@ -73,10 +123,17 @@ class Akka_headless_wp_akka_blocks {
     $akka_component_name = self::get_block_component_name(
         $blockType
     );
-    $props = self::get_block_props(
-        $blockType,
-        $attributes
-    );
+    if (str_starts_with($blockType, "splx/")) {
+        $props = self::get_splx_block_props(
+            $blockType,
+            $attributes
+        );
+    } else {
+        $props = self::get_block_props(
+            $blockType,
+            $attributes
+        );
+    }
     $block_response = wp_remote_post(
         AKKA_FRONTEND_INTERNAL_BASE . "/api/editor/component",
         [
