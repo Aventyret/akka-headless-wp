d14d6c273decc4d50ae17a6d71d659a3956bff98
Per Arnborg <kontakt@perarnborg.se>
2024-10-18 19:28:37 +0200

    Format scripts

--- includes/ahw-meta-fields.php
+++ includes/ahw-meta-fields.php
@@ -1,74 +1,85 @@
 <?php
 use \Akka_headless_wp_resolvers as Resolvers;
 
-class Akka_headless_wp_meta_fields {
-  private static $akka_meta_fields = [];
+class Akka_headless_wp_meta_fields
+{
+    private static $akka_meta_fields = [];
 
-  public static function register_post_meta_field($meta_field, $options = []) {
-    $meta_field = array_merge([
-        "name" => null,
-        "type" => "string",
-        "single" => true,
-        "default" => "",
-        "auth_callback" => function () {
-            return current_user_can("edit_posts");
-        },
-    ], $meta_field);
-
-    if (!$meta_field["name"]) {
-        throw new Exception("Akka meta field name missing!");
-    }
-
-    // TODO: Add image, file, html/rich text and more
-    if (!in_array($meta_field["type"], ['string', 'boolean', 'integer', 'number', 'array', 'object'])) {
-        throw new Exception("Akka meta field bad field type!");
-    }
-
-    if ($meta_field["type"] == "object" && !Resolvers::resolve_field($meta_field["properties"])) {
-        throw new Exception("Akka meta field missing properties for field type object!");
-    }
-
-    $options = array_merge([
-        "post_types" => ["post"],
-        "context" => "side",
-        "priority" => "default",
-    ], $options);
-
-    foreach($options["post_types"] as $post_type) {
-        // Register boat model meta box
-        add_action("add_meta_boxes", function () use ($post_type, $meta_field, $options) {
-            add_meta_box(
-                $meta_field["name"],
-                $meta_field["label"],
-                function () {
-                    echo '<div id="boat-model-meta"></div>';
+    public static function register_post_meta_field($meta_field, $options = [])
+    {
+        $meta_field = array_merge(
+            [
+                'name' => null,
+                'type' => 'string',
+                'single' => true,
+                'default' => '',
+                'auth_callback' => function () {
+                    return current_user_can('edit_posts');
                 },
-                $post_type,
-                $options["context"],
-                $options["priority"]
-            );
-        });
+            ],
+            $meta_field
+        );
 
-        // Register boat model post meta
-        add_action("init", function () use ($post_type) {
-            register_post_meta($post_type, "_" . $meta_field["name"], [
-                "show_in_rest" => $meta_field["type"] == "object" ? [
-                    "schema" => [
-                        "type" => "object",
-                        "properties" => $meta_field["properties"],
-                    ],
-                ] : true,
-                "type" => $meta_field["type"],
-                "single" => $meta_field["single"],
-                "default" => $meta_field["default"], // Note: Setting this to null does NOT work
-                "auth_callback" => $meta_field["auth_callback"],
-            ]);
-        });
-      }
+        if (!$meta_field['name']) {
+            throw new Exception('Akka meta field name missing!');
+        }
 
-    self::$akka_meta_fields[] = [
-        "meta_field" => $meta_field,
-        "options" => $options,
-    ];
-  }
+        // TODO: Add image, file, html/rich text and more
+        if (!in_array($meta_field['type'], ['string', 'boolean', 'integer', 'number', 'array', 'object'])) {
+            throw new Exception('Akka meta field bad field type!');
+        }
+
+        if ($meta_field['type'] == 'object' && !Resolvers::resolve_field($meta_field['properties'])) {
+            throw new Exception('Akka meta field missing properties for field type object!');
+        }
+
+        $options = array_merge(
+            [
+                'post_types' => ['post'],
+                'context' => 'side',
+                'priority' => 'default',
+            ],
+            $options
+        );
+
+        foreach ($options['post_types'] as $post_type) {
+            // Register boat model meta box
+            add_action('add_meta_boxes', function () use ($post_type, $meta_field, $options) {
+                add_meta_box(
+                    $meta_field['name'],
+                    $meta_field['label'],
+                    function () {
+                        echo '<div id="boat-model-meta"></div>';
+                    },
+                    $post_type,
+                    $options['context'],
+                    $options['priority']
+                );
+            });
+
+            // Register boat model post meta
+            add_action('init', function () use ($post_type) {
+                register_post_meta($post_type, '_' . $meta_field['name'], [
+                    'show_in_rest' =>
+                        $meta_field['type'] == 'object'
+                            ? [
+                                'schema' => [
+                                    'type' => 'object',
+                                    'properties' => $meta_field['properties'],
+                                ],
+                            ]
+                            : true,
+                    'type' => $meta_field['type'],
+                    'single' => $meta_field['single'],
+                    'default' => $meta_field['default'], // Note: Setting this to null does NOT work
+                    'auth_callback' => $meta_field['auth_callback'],
+                ]);
+            });
+        }
+
+        self::$akka_meta_fields[] = [
+            'meta_field' => $meta_field,
+            'options' => $options,
+        ];
+    }
 }
--- includes/ahw-resolvers.php
+++ includes/ahw-resolvers.php
@@ -1,128 +1,137 @@
 <?php
 use \Akka_headless_wp_content as Content;
 
-class Akka_headless_wp_resolvers {
-  public static function resolve_post_base($post) {
-    return [
-      "url" => \Akka_headless_wp_utils::parseUrl(
-          get_permalink($post->ID)
-      ),
-      "post_id" => $post->ID,
-      "post_title" => $post->post_title,
-      "post_type" => $post->post_type,
-      "slug" => $post->post_name,
-    ];
-  }
+class Akka_headless_wp_resolvers
+{
+    public static function resolve_post_base($post)
+    {
+        return [
+            'url' => \Akka_headless_wp_utils::parseUrl(get_permalink($post->ID)),
+            'post_id' => $post->ID,
+            'post_title' => $post->post_title,
+            'post_type' => $post->post_type,
+            'slug' => $post->post_name,
+        ];
+    }
 
-  public static function resolve_post_base_by_post_id($post_id) {
-    $post = get_post($post_id);
-    return self::resolve_post_base($post);
-  }
+    public static function resolve_post_base_by_post_id($post_id)
+    {
+        $post = get_post($post_id);
+        return self::resolve_post_base($post);
+    }
 
-  // post_data_or_fields is either an array with "fields" as key or it is the fields array
-  public static function resolve_field($post_data_or_fields, $field_name) {
-    $fields = NULL;
-    if (!isset($post_data_or_fields[$field_name]) && isset($post_data_or_fields["fields"])) {
-      $fields = $post_data_or_fields["fields"];
-    } else if (is_array($post_data_or_fields)) {
-      $fields = $post_data_or_fields;
+    // post_data_or_fields is either an array with "fields" as key or it is the fields array
+    public static function resolve_field($post_data_or_fields, $field_name)
+    {
+        $fields = null;
+        if (!isset($post_data_or_fields[$field_name]) && isset($post_data_or_fields['fields'])) {
+            $fields = $post_data_or_fields['fields'];
+        } elseif (is_array($post_data_or_fields)) {
+            $fields = $post_data_or_fields;
+        }
+        if (!isset($fields[$field_name]) || empty($fields[$field_name])) {
+            return null;
+        }
+        return $fields[$field_name];
     }
-    if (!isset($fields[$field_name]) || empty($fields[$field_name])) {
-      return NULL;
-    }
-    return $fields[$field_name];
-  }
 
-  public static function resolve_boolean_field($post_data_or_fields, $field_name) {
-    $field = self::resolve_field($post_data_or_fields, $field_name);
-    if (!$field) {
-      return FALSE;
+    public static function resolve_boolean_field($post_data_or_fields, $field_name)
+    {
+        $field = self::resolve_field($post_data_or_fields, $field_name);
+        if (!$field) {
+            return false;
+        }
+        return true;
     }
-    return TRUE;
-  }
 
-  public static function resolve_link_field($post_data_or_fields, $field_name) {
-    $field = self::resolve_field($post_data_or_fields, $field_name);
-    if (!$field) {
-      return NULL;
+    public static function resolve_link_field($post_data_or_fields, $field_name)
+    {
+        $field = self::resolve_field($post_data_or_fields, $field_name);
+        if (!$field) {
+            return null;
+        }
+        return [
+            'text' => $field['title'],
+            'url' => $field['url'],
+            'target' => $field['target'],
+        ];
     }
-    return [
-      "text" => $field["title"],
-      "url" => $field["url"],
-      "target" => $field["target"],
-    ];
-  }
 
-  public static function resolve_array_field($post_data_or_fields, $field_name) {
-    $field = self::resolve_field($post_data_or_fields, $field_name);
-    if (!$field) {
-      return [];
+    public static function resolve_array_field($post_data_or_fields, $field_name)
+    {
+        $field = self::resolve_field($post_data_or_fields, $field_name);
+        if (!$field) {
+            return [];
+        }
+        return $field;
     }
-    return $field;
-  }
 
-  public static function resolve_post_field($post_data_or_fields, $field_name) {
-    $field = self::resolve_field($post_data_or_fields, $field_name);
-    if (!$field) {
-      return NULL;
+    public static function resolve_post_field($post_data_or_fields, $field_name)
+    {
+        $field = self::resolve_field($post_data_or_fields, $field_name);
+        if (!$field) {
+            return null;
+        }
+        if (is_numeric($field)) {
+            $field = get_post($field);
+        }
+        return Content::get_post_in_archive($field);
     }
-    if (is_numeric($field)) {
-      $field = get_post($field);
-    }
-    return Content::get_post_in_archive($field);
-  }
 
-  public static function resolve_posts_field($post_data_or_fields, $field_name) {
-    $field = self::resolve_field($post_data_or_fields, $field_name);
-    if (!$field) {
-      return [];
-    }
-    return array_map(
-        function ($post_id) {
+    public static function resolve_posts_field($post_data_or_fields, $field_name)
+    {
+        $field = self::resolve_field($post_data_or_fields, $field_name);
+        if (!$field) {
+            return [];
+        }
+        return array_map(function ($post_id) {
             $p = get_post($post_id);
             return Content::get_post_in_archive($p);
-        },
-        $field
-    );
-  }
-
-  public static function resolve_global_field($field_name) {
-    return get_field(
-        "global_" . $field_name,
-        "global"
-    );
-  }
-
-  public static function resolve_image_field($post_data_or_fields, $field_name, $size = "full") {
-    $field = self::resolve_field($post_data_or_fields, $field_name);
-    return $field ? self::resolve_image($field, $size) : NULL;
-  }
-
-  public static function resolve_wysiwyg_field($post_data_or_fields, $field_name) {
-    return \Akka_headless_wp_utils::parseWysiwyg(self::resolve_field($post_data_or_fields, $field_name));
-  }
-
-  public static function resolve_image($image_id, $size = "full", $include_caption = false) {
-    if (!$image_id) {
-      return NULL;
+        }, $field);
     }
-    $post_image_attributes = \Akka_headless_wp_utils::internal_img_attributes($image_id,
-      [
-          "size" => $size,
-      ], $include_caption
-    );
-    if (empty($post_image_attributes)) {
-        return NULL;
+
+    public static function resolve_global_field($field_name)
+    {
+        return get_field('global_' . $field_name, 'global');
     }
-    return $post_image_attributes;
-  }
 
-  public static function resolve_image_with_caption($image_id, $size = "full") {
-    return self::resolve_image($image_id, $size = "full", true);
-  }
+    public static function resolve_image_field($post_data_or_fields, $field_name, $size = 'full')
+    {
+        $field = self::resolve_field($post_data_or_fields, $field_name);
+        return $field ? self::resolve_image($field, $size) : null;
+    }
 
-  public static function resolve_post_image($post_id, $size = "full") {
-    $post_image_id = get_post_thumbnail_id($post_id);
-    return self::resolve_image($post_image_id, $size);
-  }
+    public static function resolve_wysiwyg_field($post_data_or_fields, $field_name)
+    {
+        return \Akka_headless_wp_utils::parseWysiwyg(self::resolve_field($post_data_or_fields, $field_name));
+    }
+
+    public static function resolve_image($image_id, $size = 'full', $include_caption = false)
+    {
+        if (!$image_id) {
+            return null;
+        }
+        $post_image_attributes = \Akka_headless_wp_utils::internal_img_attributes(
+            $image_id,
+            [
+                'size' => $size,
+            ],
+            $include_caption
+        );
+        if (empty($post_image_attributes)) {
+            return null;
+        }
+        return $post_image_attributes;
+    }
+
+    public static function resolve_image_with_caption($image_id, $size = 'full')
+    {
+        return self::resolve_image($image_id, $size = 'full', true);
+    }
+
+    public static function resolve_post_image($post_id, $size = 'full')
+    {
+        $post_image_id = get_post_thumbnail_id($post_id);
+        return self::resolve_image($post_image_id, $size);
+    }
 }
